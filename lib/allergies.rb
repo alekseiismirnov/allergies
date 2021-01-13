f7f05c9b8cc2577f153e0ac95582d8d7bc81c43d
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-13 15:27:40 +0200

    output format bag fixed

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,5 +1,5 @@
 ALLERGENS = %w[eggs peanuts shellfish strawberries tomatoes chocolate pollen cats]
 def allergies n
-  return ALLERGENS[Math.log2(n).to_i]
+  return [ALLERGENS[Math.log2(n).to_i]]
 end
 
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -3,14 +3,14 @@ require 'allergies'
 
 describe 'allergies' do
   it 'returns `eggs` on 1' do
-    expect(allergies(1)).to eq('eggs')
+    expect(allergies(1)).to eq(['eggs'])
   end
 
   it 'returns `peanuts` on 2' do
-    expect(allergies(2)).to eq('peanuts')
+    expect(allergies(2)).to eq(['peanuts'])
   end
 
   it 'returns `tomatoes` on 16' do
-    expect(allergies(16)).to eq('tomatoes')
+    expect(allergies(16)).to eq(['tomatoes'])
   end
 end
